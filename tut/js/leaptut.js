516cc15a89e03252518cc4a46ba8fa19fb8a2976
Lars Johnson <larsj@mit.edu>
2014-04-05 10:17:19 -0400

    Hand positions + tap gestures

--- tut/js/leaptut.js
+++ tut/js/leaptut.js
@@ -1,10 +1,13 @@
 var canvas = document.getElementById('canvas');
 var c = canvas.getContext('2d');
 
-var controller = new Leap.Controller();
+var controller = new Leap.Controller({enableGestures: true});
 
 var canvasWidth;
 var canvasHeight;
+
+// Per timest
+var frame;
 
 // Setup Leap events.
 function onControllerConnect() {
@@ -26,7 +29,7 @@ function onLeapReady() {
     canvasHeight = canvas.clientWidth;
 }
 
-function leapToScene(frame, leapPos) {
+function leapToScene(leapPos) {
     var iBox = frame.interactionBox;
 
     var left = iBox.center[0] - iBox.size[0] / 2;
@@ -42,7 +45,6 @@ function leapToScene(frame, leapPos) {
 }
 
 function drawConnection(handPos, fingerPos) {
-    console.log({h: handPos, f: fingerPos});
     c.strokeStyle = '#FFA040';
     c.lineWidth = 3;
     c.beginPath();
@@ -70,23 +72,107 @@ function drawHand(handPos) {
     c.fill();
 }
 
-function onAnimationFrame(frame) {
+var keyTaps = [];
+var KEYTAP_LIFETIME = 3;
+var KEYTAP_START_SIZE = 15;
+
+function onTap(gesture) {
+    var pos = leapToScene(gesture.position);
+    var time = frame.timestamp;
+    keyTaps.push({pos: [pos[0], pos[1]], time: time, type: gesture.type});
+}
+
+function updateKeyTaps() {
+    for (var i = 0; i < keyTaps.length; i++) {
+        var keyTap = keyTaps[i];
+        var age = (frame.timestamp - keyTap.time) / 1000000;
+        if ( age > KEYTAP_LIFETIME) {
+            keyTaps.splice(i, 1);
+        }
+    }
+}
+
+function drawKeyTaps() {
+    for (var i = 0; i < keyTaps.length; i++) {
+        var keyTap = keyTaps[i];
+
+        var x = keyTap.pos[0];
+        var y = keyTap.pos[1];
+
+        var age = (frame.timestamp - keyTap.time) / 1000000;
+
+        var completion = age / KEYTAP_LIFETIME;
+        var timeLeft = 1 - completion;
+
+        c.strokeStyle = "#FF2300";
+        c.lineWidth = 3;
+
+        c.beginPath();
+        c.arc(x, y, KEYTAP_START_SIZE, 0, Math.PI * 2);
+        c.closePath();
+        c.stroke();
+
+        var opacity = timeLeft;
+        var radius = Math.max(KEYTAP_START_SIZE * timeLeft, 0);
+
+        c.fillStyle = "rgba(256, 33, 0, " + opacity + ")";
+        
+        c.beginPath();
+        c.arc(x, y, radius, 0, Math.PI * 2);
+        c.closePath();
+        c.fill();
+    }
+}
+
+function displayText(message) {
+    // Defines the font shape and size
+    c.font = "30px Helvetica";
+    
+    // Tells Canvas how to align text
+    c.textAlign = 'center';
+    c.textBaseline = 'middle';
+    
+    // Tells Canvas to draw the The number of fingers,
+    // at the center of the canvas
+    c.fillText(message , canvasWidth/2 , canvasHeight * 2 / 3 );
+ }
+
+function onAnimationFrame(newFrame) {
+    frame = newFrame;
     var numberOfFingers = frame.fingers.length;
 
     c.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    if (frame.hands.length > 1) {
+        displayText("Error: ensure only one hand is over the controller.");
+        return false;
+    }
     
     for (var i = 0; i < frame.hands.length; i++) {
         var hand = frame.hands[i];
-        var handPos = leapToScene(frame, hand.palmPosition);
+        var handPos = leapToScene(hand.palmPosition);
 
         for (var j = 0; j < hand.fingers.length; j++) {
             var finger = hand.fingers[j];
-            var fingerPos = leapToScene(frame, finger.tipPosition);
+            var fingerPos = leapToScene(finger.tipPosition);
             drawConnection(handPos, fingerPos);
             drawFinger(fingerPos);
         }
         drawHand(handPos);
     }
+
+    for (var i = 0; i < frame.gestures.length; i++) {
+        var gesture = frame.gestures[i];
+        switch (gesture.type) {
+        case "keyTap":
+        case "screenTap":
+            onTap(gesture);
+            break;
+        }
+    }
+
+    updateKeyTaps();
+    drawKeyTaps();
 }
 
 controller.on('connect', onControllerConnect);
